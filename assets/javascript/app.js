43e358b17c53a5689cbee0fda0be44373ba4e8d0
Patrick Urbankowski <purbankowski@gmail.com>
2020-05-03 03:35:17 -0400

    added js that cycles through questions but is not recognizing correct input

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,42 +1,165 @@
-var triviaArr = {
+//Define variables
 
-    questions: {
-        q1: "What does apple start with?",
-        q2: "What does banana start with?",
-        q3: "What does cat start with?",
-        q4: "What does dog start with?",
-        q5: "What does ear start with?",
-        q6: "What does frog start with?",
-        q7: "What does grass start with?",
-        q8: "What does hear start with?",
-        q9: "What does indigo start with?",
-        q10: "What does jingle start with?"
-    },
+var triviaArr = [
 
-    choices: {
-        q1: ["A", "B", "C", "D"],
-        q2: ["A", "B", "C", "D"],
-        q3: ["A", "B", "C", "D"],
-        q4: ["A", "B", "C", "D"],
-        q5: ["E", "F", "G", "H"],
-        q6: ["E", "F", "G", "H"],
-        q7: ["E", "F", "G", "H"],
-        q8: ["E", "F", "G", "H"],
-        q9: ["G", "H", "I", "J"],
-        q10: ["G", "H", "I", "J"],
-    },
+    {question: "What does apple start with?",
+    answers: ["A", "B", "C", "D"],
+    correctAns: "A"},
 
-    correctAnswers: {
-        q1: "A",
-        q2: "B",
-        q3: "C",
-        q4: "D",
-        q5: "E",
-        q6: "F",
-        q7: "G",
-        q8: "H",
-        q9: "I",
-        q10: "J"
+    {question: "What does banana start with?",
+    answers: ["A", "B", "C", "D"],
+    correctAns: "B"},
+
+    {question: "What does cat start with?",
+    answers: ["A", "B", "C", "D"],
+    correctAns: "C"},
+
+    {question: "What does dog start with?",
+    answers: ["A", "B", "C", "D"],
+    correctAns: "D"},
+    
+    {question: "What does ear start with?",
+    answers: ["E", "F", "G", "H"],
+    correctAns: "E"},
+
+    {question: "What does frog start with?",
+    answers: ["E", "F", "G", "H"],
+    correctAns: "F"},
+
+    {question: "What does grass start with?",
+    answers: ["E", "F", "G", "H"],
+    correctAns: "G"},
+     
+    {question: "What does hear start with?",
+    answers: ["E", "F", "G", "H"],
+    correctAns: "H"},
+
+    {question: "What does indigo start with?",
+    answers: ["G", "H", "I", "J"],
+    correctAns: "I"},
+
+    {question: "What does indigo start with?",
+    answers: ["G", "H", "I", "J"],
+    correctAns: "J"}
+    
+];
+var triviaIndex = 0;
+var totalCorrect = 0;
+var totalIncorrect = 0;
+var totalUnanswered = 0;
+var isAnswered = false;
+var timeLeft = 15;
+var intervalId;
+
+
+//Define functions
+
+function pickQuestion() {
+    isAnswered = false;
+    timeLeft = 15;//reset timeLeft to 15 (seconds)
+    intervalId = setInterval(timer, 1000);//timer will count down from 15 sec.
+    
+    if (isAnswered === false) {
+        timer();//start the timer if the question has not been answered
     }
 
+    var corr = triviaArr[triviaIndex].correctAns;
+    var ques = triviaArr[triviaIndex].question;
+    var answ;
+    $("#question").html(ques);
+
+    for (var i = 0; i < 4; i++) {
+        answ = triviaArr[triviaIndex].answers;
+        $("#answers").append('<p class = ansClass id=' + i + '>' + answ + '</p>');
+    }
+
+    $("p").click(function() {
+        if($(this).text() === corr) {
+            isAnswered = true;
+            $("#question").text("Correct. The answer is: " + corr);
+            userCorrect();
+        }
+
+        else {
+            isAnswered = true;
+            $("#question").text("Incorrect. The answer is: " + corr);
+            userIncorrect();
+        }
+
+    });
 }
+
+function timer() {
+    if (timeLeft === 0) {
+        isAnswered = true;
+        clearInterval(intervalId);
+        $("#question").text("The answer is: " + corr);
+        noAnswer();
+    }
+    else if (isAnswered === true) {
+        clearInterval(intervalId);
+    }
+    else {
+        timeLeft--;
+        $("#timer").text("Time remaining: " + timeLeft);
+    }
+    
+}
+
+function userCorrect() {
+    totalCorrect++;
+    endQuestion();
+}
+
+function userIncorrect() {
+    totalIncorrect++;
+    endQuestion();
+}
+
+function noAnswer() {
+    totalUnanswered++;
+    endQuestion();
+}
+
+function endQuestion() {
+
+    $(".ansClass").remove();
+    //make something that shows an image when the answer is revealed
+    triviaIndex++;
+
+    if (triviaIndex < triviaArr.length) {
+        setTimeout(function() {
+            pickQuestion();
+            //do something to remove image
+    }, 5000)}//removes question after 5 seconds
+
+    else {
+        setTimeout(function() {
+            $("#question").remove();
+            //do something to remove image
+            $("#timer").remove();
+
+            $("#answers").append('<p class = ansClass gameOver>Correct: ' + totalCorrect + '</p>');
+            $("#answers").append('<p class = ansClass gameOver>Wrong: ' + totalIncorrect + '</p>');
+            $("#answers").append('<p class = ansClass gameOver>Unanswered: ' + totalUnanswered + '</p>');
+
+            setTimeout(function() {
+                location.reload();
+            }, 7000);
+        }, 5000);
+    };
+
+}
+
+function begin() {
+    $("#start").remove();
+    totalCorrect = 0;
+    totalIncorrect = 0;
+    totalUnanswered = 0;
+    pickQuestion();
+    console.log("Begin")
+}
+
+$("#start").on("click", function() {
+    begin();
+})
