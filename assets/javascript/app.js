5533423ddfc8d07bab5312578bbdbd57ea434d92
Patrick Urbankowski <purbankowski@gmail.com>
2020-05-02 21:52:54 -0400

    added initial variables in app.js

---
+++ assets/javascript/app.js
@@ -0,0 +1,42 @@
+var triviaArr = {
+
+    questions: {
+        q1: "What does apple start with?",
+        q2: "What does banana start with?",
+        q3: "What does cat start with?",
+        q4: "What does dog start with?",
+        q5: "What does ear start with?",
+        q6: "What does frog start with?",
+        q7: "What does grass start with?",
+        q8: "What does hear start with?",
+        q9: "What does indigo start with?",
+        q10: "What does jingle start with?"
+    },
+
+    choices: {
+        q1: ["A", "B", "C", "D"],
+        q2: ["A", "B", "C", "D"],
+        q3: ["A", "B", "C", "D"],
+        q4: ["A", "B", "C", "D"],
+        q5: ["E", "F", "G", "H"],
+        q6: ["E", "F", "G", "H"],
+        q7: ["E", "F", "G", "H"],
+        q8: ["E", "F", "G", "H"],
+        q9: ["G", "H", "I", "J"],
+        q10: ["G", "H", "I", "J"],
+    },
+
+    correctAnswers: {
+        q1: "A",
+        q2: "B",
+        q3: "C",
+        q4: "D",
+        q5: "E",
+        q6: "F",
+        q7: "G",
+        q8: "H",
+        q9: "I",
+        q10: "J"
+    }
+
+}
